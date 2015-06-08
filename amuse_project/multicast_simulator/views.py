d42d825dac7d04c05abe42588413968dbf868acd
Savvas Petridis <sdp2137@columbia.edu>
2015-06-08 14:39:46 -0400

    initial

--- amuse_project/multicast_simulator/views.py
+++ amuse_project/multicast_simulator/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+def index(request): 
+    return render(request, 'multicast_simulator/index.html')
 
 # Create your views here.
