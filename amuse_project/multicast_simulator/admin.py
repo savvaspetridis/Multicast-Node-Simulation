d74eb7c452e4d95ed5c1a436fc9e92b4ad1fde70
Savvas Petridis <sdp2137@columbia.edu>
2015-06-15 14:52:50 -0400

    admin stuff

--- amuse_project/multicast_simulator/admin.py
+++ amuse_project/multicast_simulator/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Interval_pFive)
+admin.site.register(Interval_One)
+admin.site.register(Interval_Two)
+
+
