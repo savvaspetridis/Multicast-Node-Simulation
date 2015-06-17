510ec691a51875703d007b943a4d1d22f55e9364
Savvas Petridis <sdp2137@columbia.edu>
2015-06-17 16:39:16 -0400

    fb_algorithm added

--- amuse_project/multicast_simulator/forms.py
+++ amuse_project/multicast_simulator/forms.py
@@ -3,6 +3,35 @@
 from django import forms
 
 class SimulationForm(forms.Form):
-	bitRate = forms.IntegerField(label='Bit Rate')
-	updateInterval = forms.DecimalField(label='Update Interval', max_digits=2, decimal_places=1)
+	BITRATES = (
+		(6, 6),
+		(9, 9),
+		(12, 12),
+		(18, 18),
+		(24, 24),
+		(36, 36),
+		(48, 48),
+		(54, 54), 
+	)
+
+	bitRate = forms.TypedChoiceField(coerce=int, empty_value=0, label='Bit Rate', choices=BITRATES)
+
+	
+	INTERVALS = (
+		(0.5, 0.5),
+		(1.0, 1.0),
+		(2.0, 2.0),
+	)
+
+	updateInterval = forms.TypedChoiceField(coerce=float, empty_value=0.0, label='Update Interval', choices=INTERVALS)
+
+	
+	ALGORITHMS = (
+		('RAND', 'k_Random'),
+		('WORST', 'k_Worst'),
+		('AMUSE', 'Amuse'),
+	)
+	
+	fbNodeAlg = forms.ChoiceField(label='Feedback Node Algorithm', required=False, choices=ALGORITHMS)
+
 
