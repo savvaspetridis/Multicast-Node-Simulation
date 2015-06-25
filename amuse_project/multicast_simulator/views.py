f4e81f3227aaead1283251bceb09a3fea87f306f
Savvas Petridis <sdp2137@columbia.edu>
2015-06-25 16:50:57 -0400

    radical views change

--- amuse_project/multicast_simulator/views.py
+++ amuse_project/multicast_simulator/views.py
@@ -12,7 +12,81 @@ from .models import *
 from .forms import *
 import random
 import math
+import json
 
+
+def index(request):
+
+    # if post request:
+    if request.method == 'POST':
+        form = SimulationForm(request.POST)
+
+        # check if the input is valid:
+        if form.is_valid(): 
+
+            # get entered br, time_interval, fb_algorithm, k, and distance (for amuse)
+            br = form.cleaned_data['bitRate']
+            time_interval = form.cleaned_data['updateInterval']
+            fb_algorithm = form.cleaned_data['fbNodeAlg']
+            k = form.cleaned_data['k']
+            dist = form.cleaned_data['d']
+
+            c = RequestContext(request, {
+                    'k': k,
+                    'dist': dist,
+                    'bit_rate': br,
+                    'interval': time_interval,
+                    'fb_alg': fb_algorithm,
+                    'form': form                       
+                })
+                
+            return render(request, 'multicast_simulator/index.html', c) 
+
+
+def get_ret_slide(request):
+    
+    if request.is_ajax():
+        if request.method == 'POST':
+            received_json_data = json.loads(request.body)
+            
+            br = freceived_json_data['b_rate']
+            time_interval = received_json_data['updateInterval']
+            fb_algorithm = received_json_data['Algorithm']
+            interv_count = received_json_data['count']
+            k = received_json_data['k']
+            d = received_json_data['dist']
+
+            if time_interval == .5:
+
+                all_nodes = Interval_pFive.objects.filter(bit_rate=br)
+                ret_slide = create_ret_slide(interv_count, br, all_nodes)
+                fb_nodes = run_feedback_alg(fb_algorithm, interv_count, k, dist, nodes)
+
+                resp_data = {'pdr_set': 'ret_slide', 'feedback_set': 'fb_nodes', 'bit_rate': 'br'}
+                return JsonResponse(resp_data)
+
+            elif time_interval == 1:
+
+                all_nodes = Interval_One.objects.filter(bit_rate=br)
+                ret_slide = create_ret_slide(interv_count, br, all_nodes)
+                fb_nodes = run_feedback_alg(fb_algorithm, interv_count, k, dist, nodes)
+
+                resp_data = {'pdr_set': 'ret_slide', 'feedback_set': 'fb_nodes', 'bit_rate': 'br'}
+                return JsonResponse(resp_data)
+
+            #if time_interval == 2: 
+            else:
+
+                all_nodes = Interval_Two.objects.filter(bit_rate=br)
+                ret_slide = create_ret_slide(interv_count, br, all_nodes)
+                fb_nodes = run_feedback_alg(fb_algorithm, interv_count, k, dist, nodes)
+
+                resp_data = {'pdr_set': 'ret_slide', 'feedback_set': 'fb_nodes', 'bit_rate': 'br'}
+                return JsonResponse(resp_data)
+
+
+
+'''
 def index(request):
     
     # if user inputs data: 
@@ -83,43 +157,38 @@ def index(request):
         form = SimulationForm()
 
     return render(request, 'multicast_simulator/start.html', {'form': form})
+'''
 
-def create_interv_list(timeInt, bitRate, allNodes):
-    # 'slides' is the number of updates for the interval
-    slides = int((8/timeInt)) 
+def create_ret_slide(bitRate, allNodes):
     # create list containing six lists, each containing 20 lists with 20 elements each
-    interv_list = [[[0 for c in range(20)] for b in range(20)] for a in range(slides)]
+    ret_slide = [[0 for y in range(20)] for x in range(20)]
 
-    z = 0
-    while(z < slides):
-        for node in allNodes:
-            num = z + 1
-            # get the corresponding pdr value for the slide (each node has exactly the same pdr values as slides)
-            pdr_val = getattr(node, 'pdr_' + str(num))
+    for node in allNodes:
+        pdr_val = getattr(node, 'pdr_' + str(num))
             
-            # each node name is formatted as such: '1-1', '1-2', ... 
-            # split at '-' to get the x and y coordinates on test bed
-            arr = node.name.split('-')
-            x = int(arr[0])-1 
-            y = int(arr[1])-1
-            interv_list[z][x][y] = float("{0:.2f}".format(pdr_val * 100)) # insert in master array, format as %
-        z = z + 1
+        # each node name is formatted as such: '1-1', '1-2', ... 
+        # split at '-' to get the x and y coordinates on test bed
+        arr = node.name.split('-')
+        x = int(arr[0])-1 
+        y = int(arr[1])-1
+        ret_slide[x][y] = float("{0:.2f}".format(pdr_val * 100)) # insert in master array, format as %
     
-    return interv_list
+    return ret_slide
 
-def run_feedback_alg(func_name, time_interval, k, br, dist, nodes):
+def run_feedback_alg(func_name, count, k, dist, nodes):
     slides = int((8/time_interval))
     if func_name == 'NONE':
-        ret_arr = [[[0 for z in range(20)] for y in range(20)] for x in range(slides)]
+        ret_arr = [[0 for z in range(20)] for y in range(20)]
         return ret_arr
     elif func_name == 'RAND':
-        return k_random(br, slides, k, nodes)
+        return k_random(k, nodes)
     elif func_name == 'WORST':
-        return k_worst(br, slides, k, nodes)
+        return k_worst(k, nodes, count)
     else:
-        return amuse(br, slides, dist, nodes)
+        return amuse(dist, nodes, count)
 
 # randomly choose k nodes with any pdr's (other than 0) as the feedback nodes
+'''
 def k_random(br, slides, k, all_nodes):
     ret_arr = [[[0 for z in range(20)] for y in range(20)] for x in range(slides)]
     random.seed()
@@ -138,7 +207,25 @@ def k_random(br, slides, k, all_nodes):
         z = z + 1
 
     return ret_arr
+'''
 
+def k_random(k, all_nodes):
+    ret_slide = [[0 for y in range(20)] for x in range(20)]
+    random.seed()
+
+    j = 0
+    while j < k:
+        index = random.randint(0, end)
+        arr = all_nodes[index].name.split('-')
+        x = int(arr[0])-1
+        y = int(arr[1])-1
+        ret_slide[x][y] = 1
+        j = j + 1
+
+    return ret_slide
+
+
+'''
 # choose k nodes with the lowest pdr's as the feedback nodes
 def k_worst(br, slides, k, all_nodes):
     ret_arr = [[[0 for c in range(20)] for b in range(20)] for a in range(slides)]
@@ -158,21 +245,37 @@ def k_worst(br, slides, k, all_nodes):
         count = count + 1
 
     return ret_arr
+'''
 
+def k_worst(k, all_nodes, interv_count):
+    
+    ret_slide = [[0 for y in range(20)] for x in range(20)]
+    num = str(interv_count)
+    s = all_nodes.order_by('pdr_' + num)
+    s = s[:k]
+    for node in s:
+        arr = node.name.split('-')
+        x = int(arr[0])-1
+        y = int(arr[1])-1
+        ret_slide[x][y] = 1
+
+    return ret_slide
+
+
+'''
 # amuse algorithm for choosing feedback nodes
 def amuse(br, slides, d, all_nodes):
     ret_arr = [[[0 for c in range(20)] for b in range(20)] for a in range(slides)]
     s = all_nodes
-    print('slides: ' + str(slides))
     count = 1
     while count <= slides:
         s = all_nodes
         # num is used to delineate which pdr value to take
         num = str(count)
+        # order in ascending fashion by pdr for that specific slide
+        s = s.order_by('pdr_' + num)
         # while list of all 'active' nodes != 0
         while s.count() != 0:
-            # order in ascending fashion by pdr for that specific slide
-            s = s.order_by('pdr_' + num)
             # node with worst pdr is the first node
             worst = s[0]
             arr_worst = worst.name.split('-')
@@ -180,26 +283,56 @@ def amuse(br, slides, d, all_nodes):
             x_w = int(arr_worst[0])
             y_w = int(arr_worst[1])
             ret_arr[count-1][x_w-1][y_w-1] = 1
+            s = s.exclude(name=worst.name)
             # go through the rest of the nodes
             for node in s:
-                # if this particular node is not the worst node
-                if node != worst:
-                    # get its x and y coordinates
-                    arr = node.name.split('-')
-                    x2 = int(arr[0])
-                    y2 = int(arr[1])
-                    # calculate distance using their coordinates // distance formula
-                    calc_dist = calc_distance(x_w, x2, y_w, y2)
-                    # if input distance is greater than or equal to the calculated distance
-                    if d >= calc_dist:
-                        # remove this particular node (it is within D)
-                        s = s.exclude(name=node.name)
+                # get its x and y coordinates
+                arr = node.name.split('-')
+                x2 = int(arr[0])
+                y2 = int(arr[1])
+                # calculate distance using their coordinates // distance formula
+                calc_dist = calc_distance(x_w, x2, y_w, y2)
+                # if input distance is greater than or equal to the calculated distance
+                if d >= calc_dist:
+                    # remove this particular node (it is within D)
+                    s = s.exclude(name=node.name)
             # remove current 'worst' node, so it is not chosen again
-            s = s.exclude(name=worst.name)
         count = count + 1
     return ret_arr
+'''
+
+def amuse(d, all_nodes, interv_count):
+    ret_slide = [[0 for y in range(20)] for x in range(20)]
+    num = str(interv_count)
+    s = all_nodes
+    s = s.order_by('pdr_' + num)
+
+    while s.count() != 0:
+        worst = s[0]
+        arr_worst = worst.name.split('-')
+        # get x and y coordinates of worst node
+        x_w = int(arr_worst[0])
+        y_w = int(arr_worst[1])
+        ret_slide[x_w][y_w] = 1
+        s = s.exclude(name=worst.name)
+        # go through the rest of the nodes
+        for node in s:
+            # get its x and y coordinates
+            arr = node.name.split('-')
+            x2 = int(arr[0])
+            y2 = int(arr[1])
+            # calculate distance using their coordinates // distance formula
+            calc_dist = calc_distance(x_w, x2, y_w, y2)
+            # if input distance is greater than or equal to the calculated distance
+            if d >= calc_dist:
+                # remove this particular node (it is within D)
+                s = s.exclude(name=node.name)
+
+    return ret_slide
+
 
 # distance formula
 def calc_distance(x1, x2, y1, y2):
     return math.sqrt((math.pow((x1-x2),2) + math.pow((y1-y2),2)))
+
 
