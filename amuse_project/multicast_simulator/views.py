384e1703466e52758449c479265b82a57fdaf6ff
Savvas Petridis <sdp2137@columbia.edu>
2015-07-06 13:59:23 -0400

    rand and worst are perfect

--- amuse_project/multicast_simulator/views.py
+++ amuse_project/multicast_simulator/views.py
@@ -127,39 +127,50 @@ def run_feedback_alg(func_name, count, k, dist, nodes):
         ret_arr = [[0 for z in range(20)] for y in range(20)]
         return ret_arr
     elif func_name == 'RAND':
-        return k_random(k, nodes)
+        return k_random(k, nodes, count)
     elif func_name == 'WORST':
         return k_worst(k, nodes, count)
     else:
         return amuse(dist, nodes, count)
 
-def k_random(k, all_nodes):
+def k_random(k, all_nodes, interv_count):
+    num = str(interv_count)
     ret_slide = [[0 for y in range(20)] for x in range(20)]
     random.seed()
     end = all_nodes.count()-1
 
-    j = 0
-    while j < k:
+    count = 0
+    while count < k:
         index = random.randint(0, end)
-        arr = all_nodes[index].name.split('-')
-        x = int(arr[0])-1
-        y = int(arr[1])-1
-        ret_slide[x][y] = 1
-        j = j + 1
+        rand_node = all_nodes[index]
+        pdr_val = getattr(rand_node, 'pdr_' + num)
+        if pdr_val > 0:
+            arr = rand_node.name.split('-')
+            x = int(arr[0])-1
+            y = int(arr[1])-1
+            ret_slide[x][y] = 1
+            count = count + 1
 
     return ret_slide
 
 def k_worst(k, all_nodes, interv_count):
     
+    print('hey')
     ret_slide = [[0 for y in range(20)] for x in range(20)]
     num = str(interv_count)
     s = all_nodes.order_by('pdr_' + num)
-    s = s[:k]
+    count = 0
     for node in s:
-        arr = node.name.split('-')
-        x = int(arr[0])-1
-        y = int(arr[1])-1
-        ret_slide[x][y] = 1
+        pdr_val = getattr(node, 'pdr_' + num)
+        if pdr_val > 0:
+            print(str(pdr_val))
+            arr = node.name.split('-')
+            x = int(arr[0])-1
+            y = int(arr[1])-1
+            ret_slide[x][y] = 1
+            count = count + 1
+            if count == k:
+                break
 
     return ret_slide
 
@@ -167,18 +178,32 @@ def amuse(d, all_nodes, interv_count):
     ret_slide = [[0 for y in range(20)] for x in range(20)]
     num = str(interv_count)
     s = all_nodes
+    print "order start"
     s = s.order_by('pdr_' + num)
+    print "order end"
 
     while s.count() != 0:
+        print "enter while loop of pain and misery!!"
         worst = s[0]
+        print "while loop 1"
         arr_worst = worst.name.split('-')
+        print "while loop 2"
         # get x and y coordinates of worst node
         x_w = int(arr_worst[0])
+        print "while loop 3"
         y_w = int(arr_worst[1])
+      
+
         ret_slide[x_w][y_w] = 1
+        print "while loop 5"
         s = s.exclude(name=worst.name)
+        print "while loop 6"
         # go through the rest of the nodes
+        print "enter for loop of death!!!!!!"
+        iteration = 0
         for node in s:
+            print "count: " + str(iteration)
+
             # get its x and y coordinates
             arr = node.name.split('-')
             x2 = int(arr[0])
@@ -188,9 +213,35 @@ def amuse(d, all_nodes, interv_count):
             # if input distance is greater than or equal to the calculated distance
             if d >= calc_dist:
                 # remove this particular node (it is within D)
+                print "exclude begin"
                 s = s.exclude(name=node.name)
-
+                print "exclude end"
+            iteration = iteration + 1
+    print "returned!!"
     return ret_slide
+
+'''
+# main rate adaption algorithm
+def adapt_rate(lowest_rate, w_min, interv_count):
+    rate = lowest_rate
+    window = w_min
+    change_time = interv_count
+    ref_time = interv_count
+
+
+
+
+
+# window size determination
+def get_window_size():
+
+# rate decision
+def get_rate(): 
+'''
+
+
+
+
 
 
 # distance formula
