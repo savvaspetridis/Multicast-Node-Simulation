ea3617ba0019fed449acfbc138ba1229f236ee5d
Raphael Norwitz <raphael.s.norwitz@columbia.edu>
2015-07-07 14:10:39 -0400

    made changes to separate out javascript from index.html, required a lot of changes to urls, settings, and views. These changes make start.html obsolete

--- amuse_project/multicast_simulator/urls.py
+++ amuse_project/multicast_simulator/urls.py
@@ -12,11 +12,15 @@ urlpatterns = [
 from django.conf.urls import url
 
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
     url(r'^update$', views.get_ret_slide, name='get return slide'),
+    # added for static
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root', settings.STATIC_ROOT}),
 
 
-]
+] 
